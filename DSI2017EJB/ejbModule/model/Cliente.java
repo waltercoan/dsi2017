3763fad2a34e0c801d8a00aba2c8d025ca5157d9
Walter Silvestre Coan <walter.coan@gmail.com>
2017-07-28 20:35:26 -0300

    Código inicial 26/07/2017

---
+++ DSI2017EJB/ejbModule/model/Cliente.java
@@ -0,0 +1,38 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(length=1000)
+	private String nome;
+	private int sexo;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getSexo() {
+		return sexo;
+	}
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
+	
+}
