fa0494d98ba9ffa9d71cb6add90357e5558826e7
Walter Silvestre Coan <walter.coan@gmail.com>
2017-09-13 20:41:32 -0300

    Código da aula de 13/09 implementação da tela de cadastro simples

--- dsi2017web/src/control/CrtCliente.java
+++ dsi2017web/src/control/CrtCliente.java
@@ -2,23 +2,23 @@ package control;
 
 import java.util.List;
 
-import javax.ejb.EJB;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import ejb.ClienteEJBLocal;
 import model.Cliente;
-
+import javax.ejb.EJB;
 @ManagedBean(name="crtcliente")
 @ViewScoped
 public class CrtCliente {
 	@EJB
 	private ClienteEJBLocal clienteEJB;
 	
-	
 	public List<Cliente> todosClientes(){
 		return clienteEJB.todosClientes();
 	}
+	
 	
 	
 }
